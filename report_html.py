236e5b09f202fb791ac17d08ad5788f4eb3f7fef
Ash <ads04r@ecs.soton.ac.uk>
2014-09-03 20:22:23 +0100

    Added HTML report

---
+++ report_html.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys, json, string
+
+jsontxt = sys.stdin.read().decode('utf8', 'ignore')
+
+people = json.loads(jsontxt)
+k = people.keys()
+c = len(k)
+max = 0
+for i in range(0, c):
+	id = k[i]
+	people[id]['moviecount'] = len(people[id]['movies'])
+	if people[id]['moviecount'] > max:
+		max = people[id]['moviecount']
+
+for i in range(2, max + 1):
+	print "<table>"
+
+	for j in range(0, c):
+		id = k[j]
+		if people[id]['moviecount'] == i:
+			movielist = string.capwords(', '.join(people[id]['movies']).replace("_", " "))
+			print '<tr><td><a href="http://www.imdb.com/name/nm' + id + '/">' + people[id]['name'] + '</a></td><tr>(' + movielist + ')</td></tr>'
+
+	print "</table>"
+
